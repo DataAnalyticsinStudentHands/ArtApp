948647031b5cb24eb427d9bf265faf84c14c41df
CarlSteven <carl_steven@live.com>
2015-01-07 09:56:15 -0600

    Added custom NGN for server internet unavailability.

--- www/js/app.js
+++ www/js/app.js
@@ -9,7 +9,8 @@ var publicArtApp = angular.module('publicArtApp', [
     'restangular',
     'databaseServicesModule',
     'utilModule',
-    'adaptive.googlemaps'
+    'adaptive.googlemaps',
+    'ngNotify'
 ]);
 
 
@@ -60,8 +61,8 @@ publicArtApp.config(['$stateProvider','$urlRouterProvider', '$compileProvider',
         $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|geo|maps):/);
     }]);
 
-publicArtApp.run(['$rootScope', '$http', 'Restangular', 'Auth', 'tourInfo', '$ionicSideMenuDelegate','appStateStore',
-    function($rootScope, $http, Restangular, Auth, tourInfo, $ionicSideMenuDelegate,appStateStore){
+publicArtApp.run(['$rootScope', '$http', 'Restangular', 'Auth', 'tourInfo', '$ionicSideMenuDelegate','appStateStore','ngNotify',
+    function($rootScope, $http, Restangular, Auth, tourInfo, $ionicSideMenuDelegate,appStateStore,ngNotify){
         Restangular.setBaseUrl("http://www.housuggest.org:8080/ArtApp/");
 
         Auth.setCredentials("Admin", "a91646d0a63e7511327e40cd2e31b297e8094e4f22e9c0a866549e4621bff8c190c71c7e9e9a9f40700209583130828f638247d6c080a67b865869ce902bb285");
@@ -73,8 +74,10 @@ publicArtApp.run(['$rootScope', '$http', 'Restangular', 'Auth', 'tourInfo', '$io
             // Load artwork and tours after credentials confirmed
             tourInfo.loadData();
         },function(error){
+            Auth.confirmCredentials();
             //Nothing needs to be done here.
             tourInfo.loadData();
+            ngNotify.set("Internet or Server Unavailable; Limited Functionality", {type: "error", sticky: true});
         });
         
         appStateStore.loadData();
