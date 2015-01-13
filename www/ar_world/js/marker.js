d6b23d3bb7a49f11bfbe40a2f77cd001351cd0ed
CarlSteven <carl_steven@live.com>
2015-01-13 09:23:31 -0600

    Adjusted marker.

--- www/ar_world/js/marker.js
+++ www/ar_world/js/marker.js
@@ -64,7 +64,7 @@ function Marker(poiData) {
     this.indicatorLabel = new AR.Label("Locating...", 0.04, {
         enabled: false,
         zOrder: 1,
-        offsetY: -0.085,
+        offsetY: -0.0475,
         style: {
             textColor: '#FFFFFF',
             backgroundColor: '#000000'
